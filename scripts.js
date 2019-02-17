cd9eb56894d6d14988a1c630fe00a4c062903c51
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-02-17 12:19:10 +0300

    added getElementById and parsed interger

--- scripts.js
+++ scripts.js
@@ -1,19 +1,25 @@
-var triangle = function(sideOne, sideTwo, sideThree){
+var triangle = function() {
+  var sideOne = parseInt(document.getElementById("sideOne").value);
+  var sideTwo = parseInt(document.getElementById("sideTwo").value);
+  var sideThree = parseInt(document.getElementById("sideThree").value);
   if (isTriangle(sideOne, sideTwo, sideThree)) {
     return triangleType(sideOne, sideTwo, sideThree)
   } else {
-    return "These sides do not a triangle make.";
+    return "These sides do not make a triangle";
   }
 };
 
-var triangleType = function(sideOne, sideTwo, sideThree){
-  if (sideOne===sideTwo && sideTwo===sideThree) {
+var triangleType = function(sideOne, sideTwo, sideThree) {
+  if ((sideOne === sideTwo) && (sideTwo === sideThree)) {
     return "equilateral";
-  } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
+  } else if ((sideOne === sideTwo) || (sideOne === sideThree) || (sideThree === sideTwo)) {
     return "isosceles";
   } else {
     return "scalene";
   }
-  var isTriangle = function(sideOne, sideTwo, sideThree) {
-  return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)
 };
+
+
+var isTriangle = function(sideOne, sideTwo, sideThree) {
+  return (sideOne <= (sideTwo + sideThree)) && (sideTwo <= (sideThree + sideOne)) && (sideThree <= (sideOne + sideTwo));
+}
