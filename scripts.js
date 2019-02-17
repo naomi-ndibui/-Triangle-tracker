80fb8f05563079f8052ec710625dd3d0aa8234d4
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-02-17 12:48:53 +0300

    used atom beautify to indentand space

--- scripts.js
+++ scripts.js
@@ -5,17 +5,17 @@ var triangle = function() {
   if (isTriangle(sideOne, sideTwo, sideThree)) {
     return triangleType(sideOne, sideTwo, sideThree)
   } else {
-    return alert("These sides do not make a triangle");
+    return alert("These sides do not make a triangle.");
   }
 };
 
 var triangleType = function(sideOne, sideTwo, sideThree) {
   if ((sideOne === sideTwo) && (sideTwo === sideThree)) {
-    return alert("Equilateral: All sides of the triangle are equal");
+    return alert("Equilateral: All sides of the triangle are equal.");
   } else if ((sideOne === sideTwo) || (sideOne === sideThree) || (sideThree === sideTwo)) {
-    return alert("Isosceles: Two sides of the triangle are equal");
+    return alert("Isosceles: Two sides of the triangle are equal.");
   } else {
-    return alert("scalene: None of the sides of the triangle are equal");
+    return alert("scalene: None of the sides of the triangle are equal.");
   }
 };
 
